bd283c7d0f409dec8b268cd420113d64d722d61d
refnaldy <irvanrefnaldy98@gmail.com>
2019-10-06 16:17:48 +0700

    First Init

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View} from 'react-native';
+import ListTodo from './Todo/list_todo';
+const App = () => {
+  return (
+    <View>
+      <ListTodo />
+    </View>
+  );
+};
+export default App;
