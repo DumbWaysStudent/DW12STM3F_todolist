bc177cc1d75a7a4bcac53a445daefc14fd437b60
refnaldy <irvanrefnaldy98@gmail.com>
2019-10-07 19:53:24 +0700

    editing number 3

--- todo/Index.js
+++ todo/Index.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {View, TextInput, Text, StyleSheet} from 'react-native';
-import {Button} from 'native-base';
+import {Button, Icon} from 'native-base';
 
 const App = () => {
   const [todo, setTodo] = useState([
@@ -11,12 +11,12 @@ const App = () => {
   const [input, setInput] = useState('');
 
   const addTodo = () => {
-    if (input === '') return;
-    setTodo(prev => [
-      ...prev,
-      {id: todo.length + 1, todo: input, isDone: false, edit: false},
-    ]);
+    setTodo(prev => [...prev, {id: todo.length + 1, todo: input}]);
     setInput('');
+  };
+
+  const deleteTodo = id => {
+    setTodo(prev => prev.filter(item => item.id !== id));
   };
 
   return (
@@ -27,16 +27,24 @@ const App = () => {
           onChangeText={text => setInput(text)}
           placeholder="Input Todo Task!"
           style={Style.inputText}
+          returnKeyType="next"
         />
         <Button style={Style.btnSubmit} success onPress={() => addTodo()}>
-          <Text style={{color: 'white', fontSize: 20}}>Add</Text>
+          <Text style={{color: 'white', fontSize: 20}}>ADD</Text>
         </Button>
       </View>
       <View>
         {todo.map(item => (
-          <Text style={Style.textTodo} key={item.id}>
-            {item.todo}
-          </Text>
+          <View key={item.id} style={Style.renderData}>
+            <Text style={Style.textTodo}>{item.todo}</Text>
+            <View style={[Style.textTodo, Style.textIcon]}>
+              <Icon
+                name="trash"
+                style={{color: 'red'}}
+                onPress={() => deleteTodo(item.id)}
+              />
+            </View>
+          </View>
         ))}
       </View>
     </View>
@@ -45,17 +53,34 @@ const App = () => {
 const Style = StyleSheet.create({
   textTodo: {
     fontSize: 20,
-    borderBottomWidth: 2,
-    padding: 5,
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    margin: 2,
+    width: '80%',
+  },
+  textIcon: {
+    width: '20%',
+    borderWidth: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
   inputText: {
     padding: 10,
     borderWidth: 2,
+    borderRadius: 5,
     width: '70%',
+  },
+  renderData: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 5,
   },
   btnSubmit: {
     width: '20%',
     justifyContent: 'center',
+    borderRadius: 8.6,
   },
   form: {
     flexDirection: 'row',
@@ -63,4 +88,5 @@ const Style = StyleSheet.create({
     marginTop: 20,
   },
 });
+
 export default App;
