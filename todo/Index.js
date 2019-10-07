0466c9b9f1c20a7d1af9a12209148f44988f91b0
refnaldy <irvanrefnaldy98@gmail.com>
2019-10-07 19:50:42 +0700

    editing number 2

--- todo/Index.js
+++ todo/Index.js
@@ -1,21 +1,66 @@
 import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import {View, TextInput, Text, StyleSheet} from 'react-native';
+import {Button} from 'native-base';
+
 const App = () => {
   const [todo, setTodo] = useState([
-    {id: 0, todo: 'reading'},
-    {id: 1, todo: 'swim'},
-    {id: 2, todo: 'coding'},
+    {id: 1, todo: 'reading'},
+    {id: 2, todo: 'swim'},
+    {id: 3, todo: 'coding'},
   ]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input === '') return;
+    setTodo(prev => [
+      ...prev,
+      {id: todo.length + 1, todo: input, isDone: false, edit: false},
+    ]);
+    setInput('');
+  };
 
   return (
     <View>
-      {todo.map(item => (
-        <Text key={item.id} style={{borderBottomWidth: 2, fontSize: 20}}>
-          {item.todo}
-        </Text>
-      ))}
+      <View style={Style.form}>
+        <TextInput
+          value={input}
+          onChangeText={text => setInput(text)}
+          placeholder="Input Todo Task!"
+          style={Style.inputText}
+        />
+        <Button style={Style.btnSubmit} success onPress={() => addTodo()}>
+          <Text style={{color: 'white', fontSize: 20}}>Add</Text>
+        </Button>
+      </View>
+      <View>
+        {todo.map(item => (
+          <Text style={Style.textTodo} key={item.id}>
+            {item.todo}
+          </Text>
+        ))}
+      </View>
     </View>
   );
 };
-
+const Style = StyleSheet.create({
+  textTodo: {
+    fontSize: 20,
+    borderBottomWidth: 2,
+    padding: 5,
+  },
+  inputText: {
+    padding: 10,
+    borderWidth: 2,
+    width: '70%',
+  },
+  btnSubmit: {
+    width: '20%',
+    justifyContent: 'center',
+  },
+  form: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+  },
+});
 export default App;
